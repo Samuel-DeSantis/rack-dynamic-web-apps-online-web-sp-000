0939490d261dff466ad0fd8f96be0ba0b28cd71e
Samuel DeSantis <samuel.i.desantis@gmail.com>
2021-03-15 02:58:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,8 +7,12 @@ class Application
     num2 = Kernel.rand(1..20)
     num3 = Kernel.rand(1..20)
 
+    resp.write "#{num1}\n"
+    resp.write "#{num2}\n"
+    resp.write "#{num3}\n"
+
     if num1 == num2 && num2 == num3
-      resp.write "You Win"
+      resp.write "You Win" #=> puts to resp.write
     else
       resp.write "You Lose"
     end
